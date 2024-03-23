fe82461c93a8c5ea1989a1a22034e927a8329b23
Su Jin Shin <shinsujin42@gmail.com>
2024-03-23 19:03:51 -0400

    Update LandingPage
    included button for log-in and sign-up

--- simply-fit-client/src/components/LandingPage.tsx
+++ simply-fit-client/src/components/LandingPage.tsx
@@ -1,7 +1,14 @@
 function LandingPage() {
   return (
-    <div className="d-flex justify-content-center align-items-center min-vh-100">
-        <h1>SimplyFit</h1>
+    <div className="vw-100 vh-100 d-flex mx-auto justify-content-center align-items-center">
+      <div className="text-center">
+        <h1>Welcome to SimplyFit!</h1>
+        <h2 className="mt-3">Let's get started.</h2>
+        <button className="btn btn-primary mt-3">Sign Up</button>
+        <h5 className = "mt-3">
+          Existing member? <a href="">Log in</a>
+        </h5>
+      </div>
     </div>
   );
 }
