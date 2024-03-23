3841f55be247eeab93c0eb3280380370867810e9
Jason Dong <jsndong03@gmail.com>
2024-03-23 16:17:36 -0400

    Project Initialization

---
+++ simply fit server/src/main/java/com/main/SimplyFitApplication.java
@@ -0,0 +1,13 @@
+package com.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplyFitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplyFitApplication.class, args);
+	}
+
+}
