c5b3f76d892c1f8bcc21c402064abb6e264c646e
Su Jin Shin <shinsujin42@gmail.com>
2024-03-23 18:48:39 -0400

    create LandingPage base

---
+++ simply-fit-client/src/components/LandingPage.tsx
@@ -0,0 +1,8 @@
+function LandingPage() {
+  return (
+    <div className="d-flex justify-content-center align-items-center min-vh-100">
+        <h1>SimplyFit</h1>
+    </div>
+  );
+}
+export default LandingPage;
