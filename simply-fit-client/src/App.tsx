d5adb60a2723f5e8e6a0830d93d587dd21733118
Leo Lee <leosungwonlee@gmail.com>
2024-03-23 19:57:49 -0400

    wrote hello leo

--- simply-fit-client/src/App.tsx
+++ simply-fit-client/src/App.tsx
@@ -1,9 +1,10 @@
 import Greeting from "./components/Greeting";
+import HomePage from "./components/HomePage";
 import LandingPage from "./components/LandingPage";
 function App() {
   return (
     <div className="position-relative">
-      <LandingPage />
+      <HomePage/>
     </div>
   );
 }
